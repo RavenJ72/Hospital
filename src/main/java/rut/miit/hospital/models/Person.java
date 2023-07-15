586724c00800780443849b35c5b9a7b16cc5b918
Emoji <studyemoji@mail.ru>
2023-07-16 00:48:37 +0300

    Modified Person

--- src/main/java/rut/miit/hospital/models/Person.java
+++ src/main/java/rut/miit/hospital/models/Person.java
@@ -1,5 +1,34 @@
 package rut.miit.hospital.models;
 
-public abstract class Person {
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.sql.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "persons")
+@Getter
+@Setter
+public abstract class Person {
+    @Id
+    @Column(name = "person_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name", columnDefinition = "varchar(50)", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", columnDefinition = "varchar(50)", nullable = false)
+    private String lastName;
+
+    @Column(name = "date_of_birth", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date dateOfBirth;
 }
