022260a2f34ffaef64f7a509200e8fb0dcf1f93f
Ravenn <gleb.nickolaenko@yandex.ru>
2023-07-17 20:25:30 +0300

    added ContactController

--- src/main/java/rut/miit/hospital/controllers/ContactController.java
+++ src/main/java/rut/miit/hospital/controllers/ContactController.java
@@ -1,5 +1,55 @@
 package rut.miit.hospital.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import rut.miit.hospital.dtos.ContactDto;
+import rut.miit.hospital.services.ContactService;
+
+@RestController
+@RequestMapping("/contact")
 public class ContactController {
+
+    @Autowired
+    private ContactService contactService;
+
+    @GetMapping("/")
+    Iterable<ContactDto> getAll(){
+        return contactService.getAllContacts();
+    }
+
+    @GetMapping("/{id}")
+    ContactDto getContactById(@PathVariable Integer id){
+        return contactService.getContactById(id);
+    }
+
+    @PostMapping("/addNew")
+    ContactDto newContact(@RequestBody ContactDto contactDto){
+        return contactService.addNewContact(contactDto);
+    }
+
+    @DeleteMapping("/deleteById/{id}")
+    void deleteContactById(@RequestParam Integer id){
+        contactService.deleteContact(id);
+    }
+
+    @PutMapping("/updateAddress/{id}/{address}")
+    ContactDto updateAddressById(@RequestParam Integer id,@RequestParam String address){
+        return contactService.updateAddress(id,address);
+    }
+
+    @PutMapping("/updatePhone/{id}/{phone}")
+    ContactDto updatePhoneById(@RequestParam Integer id,@RequestParam String phone){
+        return contactService.updatePhone(id,phone);
+    }
+
+
+
+
+
+
+
+
+
+
 
 }
